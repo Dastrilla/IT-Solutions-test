00a055950cdd07f11c8dc7f3d90fa497bfd4f1cb
Dastrilla <dastrilla@yandex.ru>
2024-12-11 16:23:37 +1100

    Добавление моделей в админ панель и создание миграций

--- showcars/cars/admin.py
+++ showcars/cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Car, Comment
 # Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("make","model",
+                    "year", "description", "author")
+
+@admin.register(Comment) 
+class ComentAdmin(admin.ModelAdmin):
+    list_display = ("content","created_at", "car", "author")
