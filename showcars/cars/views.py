42d65cae1166aede40e8d592a0fe0e6a1e4cc370
Dastrilla <dastrilla@yandex.ru>
2024-12-12 09:55:40 +1100

    создание urls

--- showcars/cars/views.py
+++ showcars/cars/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Car
 
 # Create your views here.
+def index(request):
+    latest = Car.objects.all()
+    output = []
+    for item in latest:
+        output.append(item.model)
+    return HttpResponse('\n'.join(output))
