81f139da61d10a35cfb3b653b5c03d7165ddf4e6
Dastrilla <dastrilla@yandex.ru>
2024-12-11 16:08:33 +1100

    litle change

--- showcars/cars/models.py
+++ showcars/cars/models.py
@@ -4,20 +4,60 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Car(models.Model):
-    make = models.CharField(max_length=100, help_text="Марка автомобиля", verbose_name="Марка")
-    model = models.CharField(max_length=100, help_text= "Модель автомобиля", verbose_name="Модель")
-    year = models.IntegerField(verbose_name="Год")
-    description = models.TextField(help_text="Описание автомобиля", verbose_name="Описание")
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    update_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата обновления")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cars", verbose_name="Автор записи")
+    make = models.CharField(
+        max_length=100,
+        help_text="Марка автомобиля",
+        verbose_name="Марка"
+        )
+    model = models.CharField(
+        max_length=100,
+        help_text= "Модель автомобиля",
+        verbose_name="Модель"
+        )
+    year = models.IntegerField(
+        verbose_name="Год"
+        )
+    description = models.TextField(
+        help_text="Описание автомобиля",
+        verbose_name="Описание"
+        )
+    create_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+        )
+    update_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата обновления"
+        )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="cars",
+        verbose_name="Автор записи"
+        )
 
     def __str__(self):
         return self.model
     
 
 class Comment(models.Model):
-    content = models.TextField(help_text="Напишите свой комментарий", verbose_name="Текст комментария")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания комментария")
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="comments", verbose_name="Машина")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", verbose_name="Автор записи")
+    content = models.TextField(
+        help_text="Напишите свой комментарий",
+        verbose_name="Текст комментария"
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания комментария"
+        )
+    car = models.ForeignKey(
+        Car,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Машина"
+        )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Автор записи"
+        )
